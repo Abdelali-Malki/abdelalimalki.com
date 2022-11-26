c287e4c3a0556a519aba1076ca373dab6a1b73c9
Abdelali Malki <abdelalimalki.ae@gmail.com>
2022-11-26 16:21:05 +0100

    abdelalimalki.com

--- src/utilities/commonUtils.js
+++ src/utilities/commonUtils.js
@@ -1,11 +1,25 @@
 import Home from "../PortfolioContainer/Home/Home";
-
+import AboutMe from "../PortfolioContainer/AboutMe/AboutMe"
+import Resume from "../PortfolioContainer/Resume/Resume";
+import Testimonial from "../PortfolioContainer/Testimonial/Testimonial";
 
 
 export const TOTAL_SCREENS = [
   {
     screen_name: "Home",
     component: Home,
+  },
+  {
+    screen_name: "AboutMe",
+    component: AboutMe,
+  },
+  {
+    screen_name: "Resume",
+    component: Resume,
+  },
+  {
+    screen_name: "Testimonial",
+    component: Testimonial,
   },
 ];
 
